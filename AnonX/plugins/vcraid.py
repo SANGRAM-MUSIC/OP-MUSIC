5fa423b120f6c4bb7e47b1da10dab16ee3606329
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-09 11:50:30 +0530

    Update vcraid.py

--- AnonX/plugins/vcraid.py
+++ AnonX/plugins/vcraid.py
@@ -72,7 +72,7 @@ async def vcraid(_, e: Message):
             await e.reply_text(f"**> Raiding in:** {chat.title} \n\n**> Audio:** {songname} \n**> Position:** Ongoing Raid")
 
 
-@app.on_message(filters.user(SUDO_USERS) & filters.command(["vraid"], prefixes=HNDLR))
+@app.on_message(filters.user(SUDOERS) & filters.command(["vraid"], prefixes=HNDLR))
 async def vraid(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -109,7 +109,7 @@ async def vraid(_, e: Message):
             await e.reply_text(f"**> Raiding in:** {chat.title} \n\n**> Video:** {songname} \n**> Position:** Ongoing Raid")
 
 
-@app.on_message(filters.user(SUDO_USERS) & filters.command(["raidend"], prefixes=HNDLR))
+@app.on_message(filters.user(SUDOERS) & filters.command(["raidend"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -138,7 +138,7 @@ async def ping(_, e: Message):
         await e.reply_text("**No ongoing raid!**")
 
 
-@app.on_message(filters.user(SUDO_USERS) & filters.command(["raidpause"], prefixes=HNDLR))
+@app.on_message(filters.user(SUDOERS) & filters.command(["raidpause"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -167,7 +167,7 @@ async def ping(_, e: Message):
         await e.reply_text("**No ongoing raid!**")
 
 
-@app.on_message(filters.user(SUDO_USERS) & filters.command(["raidresume"], prefixes=HNDLR))
+@app.on_message(filters.user(SUDOERS) & filters.command(["raidresume"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
