321aa738a567af304c3cd3d8250e086c90e12200
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-09 12:14:27 +0530

    Update vcraid.py

--- AnonX/plugins/vcraid.py
+++ AnonX/plugins/vcraid.py
@@ -72,7 +72,7 @@ async def vcraid(_, e: Message):
             await e.reply_text(f"**> Raiding in:** {chat.title} \n\n**> Audio:** {songname} \n**> Position:** Ongoing Raid")
 
 
-@app.on_message(filters.user(SUDOERS) & filters.command(["raidend"]))
+@app.on_message(filters.user(SUDOERS) & filters.command(["vraid"]))
 async def vraid(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
