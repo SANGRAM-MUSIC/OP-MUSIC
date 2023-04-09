d45eb54a9c12b01205af928d0cb9953d020ac5d6
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-09 11:49:56 +0530

    Update vcraid.py

--- AnonX/plugins/vcraid.py
+++ AnonX/plugins/vcraid.py
@@ -38,7 +38,7 @@ aud_list = [
 
 
 
-@app.on_message(filters.user(SUDO_USERS) & filters.command(["vcraid"], prefixes=HNDLR))
+@app.on_message(filters.user(SUDOERS) & filters.command(["vcraid"], prefixes=HNDLR))
 async def vcraid(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
